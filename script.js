9fda27374252d199fe0cc2e0ea17b421a84926c8
fayiza beegam <fayizabeegam12@gmail.com>
2022-05-29 16:07:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// navbar-custom js
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar-custom').addClass("sticky");
+        } else {
+            $('.navbar-custom').removeClass("sticky");
+        }
+    });
+
+    
+    
+
+
+
+
+
+
+
+});
